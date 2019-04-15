53bb27c8bb1143b771bdea96da4d22f1ee3ba799
mazixiao <1598605372@qq.com>
2019-04-15 21:39:05 +0800

    第一次提交

---
+++ src/assets/js/index.js
@@ -0,0 +1,9 @@
+
+export default {
+  name: 'index',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
---
+++ src/components/index.vue
@@ -0,0 +1,32 @@
+
+<style lang="sass">  
+  @import '../assets/scss/index.scss';
+</style>
+
+
+
+<template>
+  <div class="hello">
+  {{msg}}
+
+    <el-carousel :interval="4000" type="card" height="200px">
+        <el-carousel-item v-for="item in 6" :key="item">
+          <h3>{{ item }}</h3>
+        </el-carousel-item>
+    </el-carousel>
+
+
+    <el-button>默认按钮</el-button>
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="text">文字按钮</el-button>
+
+
+  </div>
+</template>
+
+
+<script src='../assets/js/index.js'></script>
+
+
+
+
