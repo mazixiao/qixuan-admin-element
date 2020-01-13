eb93a0f22448faedfc92f446eb1fc10cd9063d65
Your Name <you@example.com>
2020-01-13 19:40:50 +0800

    左侧导航

---
+++ src/views/index.vue
@@ -0,0 +1,36 @@
+
+
+ 
+<template>
+
+
+  <div class="index">
+  	<commonHeader></commonHeader>
+
+  </div>
+
+
+
+
+</template>
+
+<script>
+    import commonHeader from '../components/header'
+    export default {
+        name: "index",
+        components: {
+            commonHeader
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
+
+
+
+
+
+
