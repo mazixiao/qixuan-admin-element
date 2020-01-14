484dc8f7f4ba23a3800b482d224c284a3d390f83
mazixiao <1598605372@qq.com>
2020-01-14 17:53:41 +0800

    提交

--- src/components/index.vue
+++ src/components/index.vue
@@ -127,7 +127,12 @@
     </el-menu>
 
 
-
+    <el-breadcrumb style="padding-left: 300px" separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>活动管理</el-breadcrumb-item>
+      <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+      <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+    </el-breadcrumb>
 
 
   </div>
